f2ad64d0771bf86448744e0fce772bcc5178f1d5
Dan W <daniel.winn@student.awesomeinc.org>
2022-09-14 02:34:33 +0000

    Set array

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const pics = ["https://github.com/DanTheHydrated/ImageGallery/blob/main/images/pic1.jpg", 
+"https://github.com/DanTheHydrated/ImageGallery/blob/main/images/pic2.jpg",
+"https://github.com/DanTheHydrated/ImageGallery/blob/main/images/pic3.jpg",
+"https://github.com/DanTheHydrated/ImageGallery/blob/main/images/pic4.jpg",
+"https://github.com/DanTheHydrated/ImageGallery/blob/main/images/pic5.jpg"]
+
 
 /* Declaring the alternative text for each image file */
 
